6c32e058149a7c96ddc97f2b4daa719341508c77
the-architect <marcel.scherf@epicteams.com>
2013-06-12 15:37:00 +0200

    improved constantize, shamelessly stolen from resque ;)

--- lib/qmin/core_ext/string.rb
+++ lib/qmin/core_ext/string.rb
@@ -7,11 +7,35 @@ unless !!defined?(ActiveSupport::Inflector)
         gsub(/([a-z\d])([A-Z])/,'\1_\2').
         tr('-', '_').
         downcase
-    end
+    end unless String.respond_to?(:underscore)
 
+    # shamelessly stolen from resque
     def constantize
-      Kernel.const_get(self)
-    end
+      names = self.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          args = Module.method(:const_defined?).arity != 1 ? [false] : []
+          next candidate if constant.const_defined?(name, *args)
+          next candidate unless Object.const_defined?(name)
+
+          # Go down the ancestors to check it it's owned
+          # directly before we reach Object or the end of ancestors.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const    if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, *args)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
+        end
+      end
+    end unless String.respond_to?(:constantize)
   end
 
 end
